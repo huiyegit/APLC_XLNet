bd68b2c2aa3a95b37bd751c9ce808b547c24f929
huiyegit <31486250+huiyegit@users.noreply.github.com>
2020-08-08 06:33:09 -0400

    Update wiki500k_eval.bash

--- code/run/wiki500k_eval.bash
+++ code/run/wiki500k_eval.bash
@@ -6,20 +6,20 @@ python main.py \
 --eval_all_checkpoints \
 --overwrite_output_dir \
 --data_dir ../data/Wiki500k \
---max_seq_length 128 \
+--max_seq_length 256 \
 --per_gpu_train_batch_size=16 \
---per_gpu_eval_batch_size=8 \
+--per_gpu_eval_batch_size=32 \
 --learning_rate_x 5e-5 \
 --learning_rate_h 1e-4 \
 --learning_rate_a 2e-3 \
---num_train_epochs 15.0 \
+--num_train_epochs 12.0 \
 --output_dir ../models/Wiki500k \
 --pos_label 274 \
---adaptive_cutoff 125252 250504 375756 \
+--adaptive_cutoff 167000 334000 \
 --div_value 2 \
 --logging_steps 15000 \
 --save_steps 15000 \
---gpu 0,1
+--gpu 0,1,2,3
 
 
 
